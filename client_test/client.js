fc2c015c59a1755320064b173a4f2fd12bdadea2
Luca Grasso <luca.grasso80@gmail.com>
2019-09-13 11:24:23 +0200

    Test web interface for WebApi

---
+++ client_test/client.js
@@ -0,0 +1,74 @@
+/ client-side js
+// run by the browser each time your view template referencing it is loaded
+
+console.log('hello world :o');
+
+let comments = [];
+let cols = [];
+
+// define variables that reference elements on our page
+const commentsList = document.getElementById('commenti');
+const commentsForm = document.forms[0];
+const commentoInput = commentsForm.elements['commento'];
+const colonninaInput = commentsForm.elements['colonnina'];
+
+const colsList = document.getElementById('colonnine');
+const colsForm = document.forms[1];
+const latitudineInput = colsForm.elements['latitudine'];
+const longitudineInput = colsForm.elements['longitudine'];
+const minpowerkwInput = colsForm.elements['minpowerkw'];
+
+// a helper function to call when our request for dreams is done
+const getCommentsListener = function() {
+  // parse our response to convert to JSON
+  comments = JSON.parse(this.responseText);
+
+  // iterate through every dream and add it to our page
+  comments.forEach( function(row) {
+    appendNewComment(row.commento + ", colonnina: " + row.colonnina);
+  });
+}
+
+const getColsListener = function() {
+  // parse our response to convert to JSON
+  cols = JSON.parse(this.responseText);
+
+  // iterate through every dream and add it to our page
+  cols.forEach( function(row) {
+    appendNewCols("Latitudine: " + row.Latitude + ", Longitudine: " + row.Longitude + ", Distanza: " + row.distance);
+  });
+}
+
+// request the dreams from our app's sqlite database
+const commentsRequest = new XMLHttpRequest();
+commentsRequest.onload = getCommentsListener;
+commentsRequest.open('get', '/getCommenti');
+commentsRequest.send();
+
+// a helper function that creates a list item for a given dream
+const appendNewComment = function(comment) {
+  const newListItem = document.createElement('li');
+  newListItem.innerHTML = comment;
+  commentsList.appendChild(newListItem);
+}
+
+const appendNewCols = function(col) {
+  const newListItem = document.createElement('li');
+  newListItem.innerHTML = col;
+  colsList.appendChild(newListItem);
+}
+
+colsForm.onsubmit = function(event) {
+  // stop our form submission from refreshing the page
+  event.preventDefault();
+
+  const colRequest = new XMLHttpRequest();
+//colRequest.onload = getColsListener;
+colRequest.open('get', '/trova-colonnine/?latitudine=' + latitudineInput.value + "&longitudine=" + longitudineInput.value + "&minpowerkw=" + minpowerkwInput.value);
+colRequest.send();
+  
+  // reset form 
+  latitudineInput.value = '';
+  longitudineInput.value = '';
+  minpowerkwInput.value = '';
+};
